6468cadc98cdda0e7b5570a57b23bb2b7ed4e7ef
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2021-06-12 01:58:40 +0800

    Add repo synchronization

--- updater/__main__.py
+++ updater/__main__.py
@@ -4,6 +4,10 @@ import flask, github_webhook, logging, os, waitress
 
 logger = logging.getLogger('waitress')
 
+def sync(url, head):
+    # Why does GitHub connect intermittently?
+    os.system(f'git fetch "{url}" "{head}" && git reset --hard FETCH_HEAD')
+
 def main():
     port = os.getenv('DATAPACK_WEBHOOK_UPDATER_PORT', '8080')
     app = flask.Flask('datapack-webhook-updater')
@@ -13,7 +17,9 @@ def main():
     def on_push(data):
         head, url = data['after'], data['repository']['clone_url']
         logger.info(f'Got push with: {url}, head: {head}')
+        sync(url, head)
 
+    logging.basicConfig(level=logging.INFO)
     waitress.serve(app, listen=f'0.0.0.0:{port}')
 
 if __name__ == '__main__': main()
