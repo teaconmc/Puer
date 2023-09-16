58663a46301f018fd80ba74a0e08f919a60a734b
zbx1425 <zbx1425@outlook.com>
2023-09-17 00:12:19 +0800

    Add blue line annoucements

--- 2023/vanilla-assets/src/main/resources/assets/mtr/s_train_suspend/sound.js
+++ 2023/vanilla-assets/src/main/resources/assets/mtr/s_train_suspend/sound.js
@@ -10,22 +10,26 @@ function playAnn(ctx, state, train) {
   if (nextIndex <= 0 || nextIndex >= stations.size()) return;
 
   var stationConfig = getStationConfig(stations, nextIndex);
-  if (stationConfig.annSound === undefined || stationConfig.annSound === "") {
+  if (stationConfig.code === undefined || stationConfig.code === "") {
     state.playingAnn.setState("");
   } else {
     var distLastStation = train.railProgress() - stations.get(nextIndex - 1).distance;
     var distNextStation = stations.get(nextIndex).distance - train.railProgress();
     var soundToPlay;
     if (distNextStation < stationConfig.arriveDistance) {
-      soundToPlay = stationConfig.annSound + "_arr";
+      if (nextIndex == stations.size() - 1) {
+        soundToPlay = stationConfig.routeStationCode + "_arr_term";
+      } else {
+        soundToPlay = stationConfig.routeStationCode + "_arr";
+      }
     } else if (distLastStation > 15) {
-      soundToPlay = stationConfig.annSound + "_next";
+      soundToPlay = stationConfig.routeStationCode + "_next";
     } else {
       soundToPlay = "";
     }
 
-    state.playingAnn.setState(soundToPlay);
-    if (state.playingAnn.stateNowFirst()) {
+    state.playingAnn.setState("mtr:ann_" + soundToPlay);
+    if (state.playingAnn.stateNowFirst() && soundToPlay != "") {
       ctx.playAnnSound(Resources.identifier(state.playingAnn.stateNow()), 1, 1);
     }
   }
