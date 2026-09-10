6e3df76a2d3c8a2b96b23204cad78f32dec44cd2
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2026-09-11 01:16:05 +0800

    Improve parkour completion logic

--- 2026/data/grack_exhibition/grack/script/finish_parkour.groovy
+++ 2026/data/grack_exhibition/grack/script/finish_parkour.groovy
@@ -10,7 +10,7 @@ import org.teacon.powertool.item.PowerToolItems
 import java.util.concurrent.TimeUnit
 
 // args:
-// BlockPos from, BlockPos to, BlockPos master_pos
+// BlockPos from, BlockPos to, BlockPos master_pos, int minutes, int seconds
 
 def source = context.source
 if (source.isPlayer()) {
@@ -30,22 +30,20 @@ if (source.isPlayer()) {
         player.sendSystemMessage(Component.literal('您未到达全部记录点，无法完成跑酷'))
         return
     }
+    def maxMinutes = args.getInt('minutes').orElse(5)
+    def maxSeconds = args.getInt('seconds').orElse(0)
     def currentTime = System.currentTimeMillis()
     data.put('is_in_parkour', false)
     player.sendSystemMessage(Component.literal('跑酷结束'))
 
-    boolean giveRune = false
-    if (currentTime - data.<Long>get('start_time') < TimeUnit.MINUTES.toMillis(5)) {
-        player.sendSystemMessage(Component.literal('恭喜你在5分钟内完成跑酷，使用背包中的碎片来前往大师盖章台吧'))
-        giveRune = true
-    } else {
+    if (currentTime - data.<Long>get('start_time') < TimeUnit.MINUTES.toMillis(maxMinutes) + TimeUnit.SECONDS.toMillis(maxSeconds)) {
         def is = player.getItemInHand(InteractionHand.MAIN_HAND)
-        def ch = data.<String>get("characters")
         if (is.is(Items.PAPER)) {
             def customName = is.getCustomName()
             if (customName != null) {
                 String s = customName.getString().toLowerCase()
                 if (s.length() == 9) {
+                    def ch = data.<String>get("characters")
                     boolean success = true
                     for (char c : s.toCharArray()) {
                         if (ch.indexOf((int) c) == -1) {
@@ -54,20 +52,17 @@ if (source.isPlayer()) {
                         }
                     }
                     if (success) {
-                        player.sendSystemMessage(Component.literal('恭喜你找到了9个字母，使用背包中的碎片来前往大师盖章台吧'))
-                        giveRune = true
+                        player.sendSystemMessage(Component.literal("恭喜你找到了9个字母，并在${minutes > 0 ? "${minutes}分" : ''}${seconds > 0 ? "${seconds}秒" : ''}内完成跑酷，使用背包中的碎片来前往大师盖章台吧"))
+                        def masterPos = ofBlockPos(args.getIntArray('master_pos'))
+                        ItemStack itemStack = new ItemStack(PowerToolItems.COMMAND_RUNE)
+                        itemStack.set(DataComponents.CUSTOM_NAME, Component.literal('前往大师盖章台'))
+                        itemStack.set(PowerToolDataComponents.CONSUME, true)
+                        itemStack.set(PowerToolDataComponents.COMMAND, "/tp @s $masterPos.x $masterPos.y $masterPos.z".toString())
+                        player.getInventory().add(itemStack)
+                        player.containerMenu.broadcastChanges()
                     }
                 }
             }
         }
-    }
-    if (giveRune) {
-        def masterPos = ofBlockPos(args.getIntArray('master_pos'))
-        ItemStack itemStack = new ItemStack(PowerToolItems.COMMAND_RUNE)
-        itemStack.set(DataComponents.CUSTOM_NAME, Component.literal('前往大师盖章台'))
-        itemStack.set(PowerToolDataComponents.CONSUME, true)
-        itemStack.set(PowerToolDataComponents.COMMAND, "/tp @s $masterPos.x $masterPos.y $masterPos.z".toString())
-        player.getInventory().add(itemStack)
-        player.containerMenu.broadcastChanges()
     }
 }
